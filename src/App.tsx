da5ecbd01f8e76e96d3666793f4bd230cd53e79a
shai <eliav.english@gmail.com>
2023-03-10 21:40:46 +0200

    basic

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
-import React, { useEffect, useRef } from "react";
-import logo from "./logo.svg";
-import { Counter } from "./features/counter/Counter";
 import "./App.css";
 import { Timeline } from "./features/timeline/Timeline";
-import { Emulator } from "./features/emulator/Emulator";
 import { Profile } from "./features/profile/Profile";
 
 function App() {
-  const vidRef = useRef(null);
-  useEffect(() => {
-    console.log(vidRef.current);
-  }, [vidRef.current]);
   return (
     <div className="App">
       <Profile />
       <Timeline />
-
-      {/* 
-        // my name
-      */}
     </div>
   );
 }
